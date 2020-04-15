0ae27b3b95c792a8d42615cc15dffbc5cccd7d47
gbprod <contact@gb-prod.fr>
2020-04-15 15:50:16 +0200

    Updates

---
+++ precache-manifest.69b33b434e6d23ee6a0fb6da84aac020.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "862ec98db62ff3b4a5f35f7196a2191c",
+    "url": "/playbook/index.html"
+  },
+  {
+    "revision": "ec6d4f6e2d93078a911f",
+    "url": "/playbook/static/css/2.9d5365b0.chunk.css"
+  },
+  {
+    "revision": "bc1437d254b078bda997",
+    "url": "/playbook/static/css/main.626f2bcb.chunk.css"
+  },
+  {
+    "revision": "ec6d4f6e2d93078a911f",
+    "url": "/playbook/static/js/2.8c7dff38.chunk.js"
+  },
+  {
+    "revision": "b578178f33e8b4baed76c7b8f7ce2e07",
+    "url": "/playbook/static/js/2.8c7dff38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "341be96e161ff78f78c1",
+    "url": "/playbook/static/js/3.5b36efe8.chunk.js"
+  },
+  {
+    "revision": "4663b05b76b1fcce2044",
+    "url": "/playbook/static/js/4.8722ea87.chunk.js"
+  },
+  {
+    "revision": "b0639a84d3c1e5f7b2ba",
+    "url": "/playbook/static/js/5.031937ef.chunk.js"
+  },
+  {
+    "revision": "d10280108bcf89326467",
+    "url": "/playbook/static/js/6.19ce3eb7.chunk.js"
+  },
+  {
+    "revision": "bc1437d254b078bda997",
+    "url": "/playbook/static/js/main.cecab2d2.chunk.js"
+  },
+  {
+    "revision": "5c9bfca6d5dc075b20eb",
+    "url": "/playbook/static/js/runtime-main.168f3afc.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/playbook/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/playbook/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/playbook/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/playbook/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/playbook/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/playbook/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
